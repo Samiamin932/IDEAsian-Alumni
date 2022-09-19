07bbb2f30443355625579f714ae96f3b4a86b25c
yhwan492 <kyhwna492@gmail.com>
2022-09-18 22:02:50 -0500

    Added login page and routing and a few gems needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :login
+
   root 'welcome#index'
+
+  get "/login", to: "login#index"
+  get "/welcome", to: "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
