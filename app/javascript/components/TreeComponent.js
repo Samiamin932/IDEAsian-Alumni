1600659587cc46a6dbc05b4c0a72f4d86eb6dde9
NickCheng0921 <nickshacheng.0921@gmail.com>
2022-10-02 12:20:36 -0500

    Enhanced Tree View

--- app/javascript/components/TreeComponent.js
+++ app/javascript/components/TreeComponent.js
@@ -5,7 +5,7 @@ import Tree from 'react-d3-tree'; //https://www.npmjs.com/package/react-d3-tree#
 // This is a simplified example of an org chart with a depth of 2.
 // Note how deeper levels are defined recursively via the `children` property.
 const orgChart = {
-  name: 'Alumni',
+  name: 'Group1',
   children: [
     {
       name: 'Mentor1',
@@ -51,10 +51,27 @@ const orgChart = {
 };
 
 class TreeComponent extends React.Component {
+  //center tree https://github.com/bkrem/react-d3-tree/issues/49
+  /*componentDidMount() {
+    //hide first i nodes https://stackoverflow.com/questions/63749469/react-component-render-after-event-listener
+    //document.getElementById('treeWrapper').firstChild.lastChild.firstChild.childNodes[0].style.opacity = 0;
+    //document.getElementsByClassName('treeWrapper')[0].style.opacity = 0;
+    let childNodes = 1;
+    let i = 0;
+    for (i = 0; i < childNodes; i++) {
+    // hides first i links
+      document.getElementsByClassName('linkBase')[i].style.opacity = 0  
+    }
+  }*/
+
   render () {
     return (
-      <div id="treeWrapper" style={{ width: '100%', height: '40em' }}>
-       <Tree data={orgChart} />
+      <div id="treeWrapper" style={{height: '40em'}}>
+        <Tree 
+          data={orgChart}
+          orientation={'vertical'}
+          pathFunc={'elbow'}
+        />
       </div>
     );
   }
---
+++ app/javascript/components/TreeMenuBar.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class TreeMenuBar extends React.Component {
+  render () {
+    return (
+    <div>
+      <p>Select a year</p>
+      <br></br>
+      <p>Select a field of study</p>
+      <br></br>
+      <p>Other filters and info</p>
+      <br></br>
+    </div>
+
+    );
+  }
+}
+
+export default TreeMenuBar
